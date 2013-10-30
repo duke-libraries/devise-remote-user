9e7dfd25bd6cfe1fb65dfe0dcc587dbba9f911dc
dchandekstark <dchandekstark@gmail.com>
2013-10-30 17:45:33 -0400

    bug fix

--- lib/devise_remote_user/model.rb
+++ lib/devise_remote_user/model.rb
@@ -10,7 +10,7 @@ module Devise::Models
         user_manager = DeviseRemoteUser::UserManager.new(env)
         user = user_manager.find_user
         if !user && Devise.remote_user_autocreate
-          user = user_manager.create_user(env)
+          user = user_manager.create_user
         end
         user
       end
